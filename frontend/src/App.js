4d3fcb3237f3f6a6788c2586bec11f5fb6fac707
majaspar <130063598+majaspar@users.noreply.github.com>
2024-01-12 13:14:15 +0000

    update App.js - added BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
