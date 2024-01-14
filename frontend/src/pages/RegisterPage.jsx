d8c7a5a9e1a2b2c937ceed71ebea4a23b1796954
majaspar <130063598+majaspar@users.noreply.github.com>
2024-01-14 21:14:44 +0000

    fix register button and add register page

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div className="RegisterPage">
+      <section>
+        {/* Left column */}
+        <aside></aside>
+
+        {/* Right column */}
+        <main>
+          <h1>Register Page</h1>
+        </main>
+      </section>
+    </div>
+  );
+}
