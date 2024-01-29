2cace7777c7be54db9efd5768bca37f77f616729
whitejsx <0x00whitejsx@gmail.com>
2024-01-29 13:22:24 +0100

    feat / added five endpoint

--- Backend_Heavy/utils/constant/connections.js
+++ Backend_Heavy/utils/constant/connections.js
@@ -1,10 +1,10 @@
-const PORT = 8087;
+const PORT = 8089;
 
 // api route 
 
-const mongodb_URI = "mongodb://localhost:27017";
+const mongodb_URI = "mongodb://localhost:27017/TASKZEN";
 const REDIS_server = ""
-const Application_version = "/api/v1/24"
+const Application_version = "/api/v27"
 
 
 module.exports ={
