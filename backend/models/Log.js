927e1e7b071932cc2e8e97a740b42d98e65aa83f
majaspar <130063598+majaspar@users.noreply.github.com>
2024-02-12 23:31:34 +0000

    add displaying task history in Log

--- backend/models/Log.js
+++ backend/models/Log.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const Log = new Schema(
   {
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    taskid: { type: Schema.Types.ObjectId, ref: "Task" },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    taskid: { type: Schema.Types.ObjectId, ref: "Task", required: true },
     date: { type: Date, default: Date.now },
-    logDescription: { type: String },
+    logDescription: { type: String, required: true },
   },
   {
     timestamps: true,
--- backend/models/Task.js
+++ backend/models/Task.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Task = new Schema ({
-    name: { type: String },
-    taskDescription: { type: String },
-    isCompleted: { type: Boolean},
+    name: { type: String, required: true },
+    taskDescription: { type: String, required: true },
+    isCompleted: { type: Boolean, required: true },
     deadline: { type: Date },
     days: [],
-    users:[{ type: Schema.Types.ObjectId, ref: 'User'}],
+    users:[{ type: Schema.Types.ObjectId, ref: 'User', required: true}],
     history: [{ type: Schema.Types.ObjectId, ref: 'Log'}],
-    subcategory: { type: Schema.Types.ObjectId, ref: 'Subcategory'},
+    subcategory: { type: Schema.Types.ObjectId, ref: 'Subcategory', required: true},
     priority: { type: Number }
 }, {
     timestamps: true,
