960cb5e3cc043e961923207426f9d0cfeffa19e6
roshan <roshanroy.rr@outlook.com>
2024-02-13 18:00:48 +0000

    add check email route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,18 @@
 const authRouter = require("express").Router();
 const { authJwt } = require("../middlewares/authJWT.js");
-const { signUp, signIn, signOut } = require("../controllers/authController");
+const {
+  signUp,
+  signIn,
+  signOut,
+  checkEmailExists,
+} = require("../controllers/authController");
 
 authRouter.post("/auth/signup", signUp);
 
 authRouter.post("/auth/signin", signIn);
 
 authRouter.post("/signout", [authJwt.verifyToken], signOut);
+
+authRouter.post("/check-email", checkEmailExists);
 
 module.exports = authRouter;
